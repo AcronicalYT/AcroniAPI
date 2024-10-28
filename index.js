fa1d2f3015a3719a7713503c6d42fb301bf544aa
AcronicalYT <acronicalbusiness@gmail.com>
2024-10-28 16:12:17 +0000

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const cors = require('cors');
 const data = require('./json/default.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json(data);
