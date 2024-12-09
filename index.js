07e207e06c8026a47ec1939190664380e69bc8d9
AcronicalYT <ace36501@hotmail.com>
2024-12-09 19:51:04 +0000

    projects

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 const PORT = 3001;
 const cors = require('cors');
 const data = require('./json/default.json');
+const projects = require('./json/projects.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.json(data);
+});
+
+app.get('/projects', (req, res) => {
+    res.json(projects);
 });
 
 app.listen(PORT, () => {
